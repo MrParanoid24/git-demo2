cdae13356adb5932f288ece43f5d5f0dcb7bf9aa
lxx <306834105@qq.com>
2019-12-22 14:31:32 +0800

    add sout 4

---
+++ git-demo2/src/com/heima/git2/Git2.java
@@ -0,0 +1,15 @@
+package com.heima.git2;
+
+public class Git2 {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+
+
+
+
+
+    }
+}
