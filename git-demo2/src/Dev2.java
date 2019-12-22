7e75e5b36f46173f72f48237bee6f738cee3e7ab
lxx <306834105@qq.com>
2019-12-22 15:44:33 +0800

    add a class dev2

---
+++ git-demo2/src/Dev2.java
@@ -0,0 +1,9 @@
+public class Dev2 {
+
+    public static void main(String[] args) {
+        System.out.println("www");
+        System.out.println("www");
+        System.out.println("www");
+        System.out.println("www");
+    }
+}
